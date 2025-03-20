a9b4b4a9d5f00287478a0ceca69850492e37dbf0
Elena <elenabougaran@gmail.com>
2025-03-20 18:02:54 +0100

    Initial Commit

---
+++ Vitesse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vitesse
+//
+//  Created by Ordinateur elena on 20/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
